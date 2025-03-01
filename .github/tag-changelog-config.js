ebf9a673d0443dc6548029336fcc9dc6d7d79232
Ismo Vuorinen <ismo@ivuorinen.net>
2025-03-02 00:27:36 +0200

    Feat/ci actions @coderabbitio (#61)
    * feat(ci): create daily releases
    * feat(ci): better splitting of security-suite steps
    * fix(ci): update new-release workflow

---
+++ .github/tag-changelog-config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  types: [
+    { types: ['feat', 'feature', 'Feat'], label: '🎉 New Features' },
+    { types: ['fix', 'bugfix', 'Fix'], label: '🐛 Bugfixes' },
+    { types: ['improvements', 'enhancement'], label: '🔨 Improvements' },
+    { types: ['perf'], label: '🏎️ Performance Improvements' },
+    { types: ['build', 'ci'], label: '🏗️ Build System' },
+    { types: ['refactor'], label: '🪚 Refactors' },
+    { types: ['doc', 'docs'], label: '📚 Documentation Changes' },
+    { types: ['config'], label: '🪛 Configuration Changes' },
+    { types: ['test', 'tests'], label: '🔍 Tests' },
+    { types: ['style', 'codestyle', 'lint'], label: '💅 Code Style Changes' },
+    { types: ['chore', 'Chore', 'deps', 'Deps'], label: '🧹 Chores' },
+    { types: ['other', 'Other'], label: 'Other Changes' },
+  ],
+
+  excludeTypes: [],
+
+  renderTypeSection: function(label, commits) {
+    let text = `\n## ${label}\n\n`
+
+    commits.forEach(commit => {
+      const scope = commit.scope ? `**${commit.scope}:** ` : ''
+      text += `- ${scope}${commit.subject}\n`
+    })
+
+    return text
+  },
+
+  renderChangelog: function(release, changes) {
+    const now = new Date()
+    const d = now.toISOString().substring(0, 10)
+    const header = `# ${release} - ${d}\n`
+    return header + changes + '\n\n'
+  },
+}
